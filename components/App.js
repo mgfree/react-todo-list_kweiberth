85d6f85a1745894e3a769032e735d51d26340e9a
Unknown <francesco.capuzzo@zoho.com>
2017-01-06 23:49:53 +0200

    Up to Part 4
    https://www.youtube.com/watch?v=DNM46IYrUcU

--- components/App.js
+++ components/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-var App = React.createClass({
+class App extends Component {
 
     render() {
         return <div>This is definetely a React app now!</div>
     }
-})
+}
 
 export default App;
